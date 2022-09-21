9a941e07d5df287456a989219941f9a1fd99ea36
Ingram <smarius@gmail.com>
2022-09-21 09:45:41 -0700

    fix whatever that was

--- index.js
+++ index.js
@@ -23,24 +23,6 @@ app.get('/endpoint-2', (req, res, next) => {
 
 });
 
-const http = require("http");
+const PORT = process.env.PORT || 5001
 
-const requestListener = function (req, res) {
-  res.end("Your IP Addresss is: " + req.socket.localAddress);
-};
-
-const server = http.createServer(requestListener);
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-app.get('/', function(req, res){
-    res.send('user ' + req.headers['user-agent']);
-});
-
-//app.listen(PORT, () => console.log('server is running on this port ${PORT}'))
-
-//app.get('/', function(req, res){
-//    res.send('user ' + req.headers['user-agent']);
-//});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
